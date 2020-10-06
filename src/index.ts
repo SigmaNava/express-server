43bc4016956b441fadd53e20a94004a8c9663ab8
SigmaNava <navasigma1@gmail.com>
2020-10-05 18:41:51 -0600

    Add types to mesage

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,22 @@ import express from 'express';
 const app =express();
 const port = 4000;
 
+type IMessage = {
+
+    message: string;
+}
+
+const message: IMessage = {
+
+    message: 'HOla MUndo'
+
+}
+
 app.get('/hello_world', (req, res)=> {
 
     res.json({
 
-        message: 'Hola Mundo'
+        message
 
     });
 
